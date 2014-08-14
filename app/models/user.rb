3a7375a274f51feafcf85351e4ee9407418d0c4e
arnaud74130 <arnaud.garcia@epsilim.fr>
2014-08-14 16:51:00 +0200

    ajout validation sur le nom

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ActiveRecord::Base
   enum role: [:etablissement, :fournisseur, :admin]
   after_initialize :set_default_role, :if => :new_record?
   belongs_to :entreprise, polymorphic: true
-  validate :nom, presence: true
+
+  validates_presence_of :nom
+  validates_uniqueness_of    :email,     :case_sensitive => false, :allow_blank => true, :if => :email_changed?
+  validates_format_of :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+  validates_presence_of   :password, :on=>:create
+  validates_confirmation_of   :password, :on=>:create
+  validates_length_of :password, :within => Devise.password_length, :allow_blank => true
 
   def set_default_role
     self.role ||= :etablissement
@@ -10,9 +16,10 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :validatable, enlever car sinon on ne peut pas ajouter des validations supplémentaires...
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
-def entreprise_nom
-  entreprise.nom if entreprise
-end
+    :recoverable, :rememberable, :trackable
+  def entreprise_nom
+    entreprise.nom if entreprise
+  end
 end
