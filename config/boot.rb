e17979724b8a424dcfe250c0d4742749cb397f98
arnaud74130 <arnaud.garcia@epsilim.fr>
2015-01-14 18:24:19 +0100

    change yam engine to syck due to ruby 2

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+# YAML Syck engine support was dropped in Ruby 2.0 and it's available only by a gem 'syck'.
+# Some gems (delayed_job[1] for instance) have YAML extensions depending on the engine.
+# Switching to the Syck engine should be done in boot.rb before loading all gems in application.rb:
+require 'yaml'
+require 'syck'
+YAML::ENGINE.yamler = 'syck'
